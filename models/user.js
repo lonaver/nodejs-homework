060bb0b64a2d6ee570e17f1fbe7b06913d5b6580
lonaver <ilonaver@ukr.net>
2023-07-20 13:51:31 +0300

    added hw4

---
+++ models/user.js
@@ -0,0 +1,50 @@
+const Joi = require("joi");
+
+const { Schema, model } = require("mongoose");
+
+const { handleMongooseError } = require("../helpers");
+
+const listSubscriptions = ["starter", "pro", "business"];
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: listSubscriptions,
+      default: "starter",
+    },
+    token: String,
+  },
+  { versionKey: false, timestamps: false }
+);
+
+userSchema.post("save", handleMongooseError);
+
+const registratorSchema = Joi.object({
+  email: Joi.string().required(),
+  password: Joi.string().required(),
+});
+const loginSchema = Joi.object({
+  email: Joi.string().required(),
+  password: Joi.string().required(),
+});
+const updateSubscriptionSchema = Joi.object({
+  subscription: Joi.string()
+    .valid(...listSubscriptions)
+    .required(),
+});
+
+const schemas = { registratorSchema, loginSchema, updateSubscriptionSchema };
+
+const User = model("user", userSchema);
+
+module.exports = { User, schemas };
