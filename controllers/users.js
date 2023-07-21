04f16bd77dca69e0707d31ff2444aba42c666c23
lonaver <ilonaver@ukr.net>
2023-07-21 15:29:24 +0300

    fixed in accordance with the comments

--- controllers/users.js
+++ controllers/users.js
@@ -7,14 +7,14 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const register = async (req, res) => {
   const { email, password } = req.body;
-  const user = User.findOne({ email });
-  if (!user) throw HttpError(409, "Email in use");
+  const user = await User.findOne({ email });
+  if (user) throw HttpError(409, "Email in use");
 
   const hashPassword = await bcrypt.hash(password, 10);
   const newUser = await User.create({ ...req.body, password: hashPassword });
-  res
-    .status(201)
-    .json({ email: newUser.email, subscription: newUser.subscription });
+  res.status(201).json({
+    user: { email: newUser.email, subscription: newUser.subscription },
+  });
 };
 
 const login = async (req, res) => {
